1fbd1bf16ec918f1275d1a3d37887d4a904897b8
koushin07 <canaresmiko3@gmail.com>
2022-10-31 08:19:23 +0800

    one module

---
+++ resources/js/Pages/municipality/TransactionPage.vue
@@ -0,0 +1,284 @@
+<template>
+<Head title="Tansaction"/>
+    <Content-box>
+        <!-- grid grid-cols-5 gap-2 -->
+
+        <!-- Requests -->
+        <div class="flex flex-col col-span-2  overflow-hidden h-full ">
+            <span class="text-lg font-semibold font-sans text-center">Requests</span>
+            <div class=" flex flex-col justify-between overflow-y-auto border-2 rounded-lg    space-y-2 scrollbar">
+                <div v-for="(notification, key ) in notifications" :key="key"
+                    class="rounded-lg p-4  relative text-center flex justify-between pr-2 border-b pb-2 border-red-200 last:border-transparent">
+
+
+
+                    <div class="grid grid-cols-1">
+
+                        <span class="font-bold text-sm text-gray-700 text-start">{{
+                                notification.data.borrower[0].municipality
+                        }}</span>
+                        <button class="font-bold text-sm text-gray-600 text-start hover:text-slate-900">{{
+                                notification.data.equipment
+                        }}:
+                            <span class="text-xs">{{ notification.data.quantity }}</span>
+                        </button>
+                    </div>
+
+                    <div class="flex flex-col justify-between space-y-2">
+                        <button
+                            @click="accept(notification.data.equipment, notification.data.borrower_id, notification.id, notification.data.borrower[0].municipality)"
+                            class=" text-sm  hover:bg-green-600  text-center bg-green-500 px-2 rounded-lg text-white tracking-wide">Accept</button>
+                        <button
+                            @click="deny(notification.data.equipment, notification.data.borrower_id, notification.id, notification.data.borrower[0].municipality)"
+                            class="text-sm  hover:bg-red-600  text-center bg-red-500 px-2 rounded-lg text-white tracking-wide">Deny
+
+                        </button>
+                    </div>
+
+                </div>
+
+
+
+            </div>
+
+
+        </div>
+        <!-- End of Request -->
+        <!-- Equipments -->
+        <!-- <div class="flex flex-col col-span-3  overflow-hidden h-[280px] ">
+                    <span class="text-lg font-semibold font-sans text-center">Requests</span>
+                    <div
+                        class="flex  flex-col justify-between overflow-y-auto border-2 rounded-lg space-y-2 scrollbar">
+                        <button
+                            class="hover:bg-orange-300 rounded-lg p-4  relative text-center flex justify-between pr-2 border-b pb-2 border-red-200 last:border-transparent">
+
+                         
+                            <div class="grid grid-cols-1">
+                                <span class="text-xs text-gray-700 text-start">Equipment</span>
+                                <span class="font-bold text-sm text-gray-700 ">Assault Rescue Harness
+                                   
+                                </span>
+                            </div>
+                            
+                            <div class="grid grid-cols-1">
+                                <span class="text-xs text-gray-700 text-start">Availabe</span>
+                                <span class="font-bold text-sm text-gray-700 ">123
+                                   
+                                </span>
+                            </div>
+                            <div class="grid grid-cols-1">
+                                <span class="text-xs text-gray-700 text-start">Serial</span>
+                                <span class="font-bold text-sm text-gray-700 ">3123
+                                   
+                                </span>
+                            </div>
+                            <div class="grid grid-cols-1">
+                                <span class="text-xs text-gray-700 text-start">Model</span>
+                                <span class="font-bold text-sm text-gray-700 ">3123
+                                   
+                                </span>
+                            </div>
+                            <div class="grid grid-cols-1">
+                                <span class="text-xs text-gray-700 text-start">Asset</span>
+                                <span class="font-bold text-sm text-gray-700 ">3123
+                                   
+                                </span>
+                            </div>
+
+                        </button>
+                     
+                       
+
+
+                    </div>
+                </div> -->
+        <!-- End of Equipment -->
+
+
+
+
+    </Content-box>
+    <Content-box>
+        <div class="grid grid-cols-5 gap-5 place-content-center">
+            <div class=" flex flex-col z-0  justify-between col-span-2">
+                <div class="flex flex-col  overflow-hidden h-[280px] ">
+
+                    <div class="flex flex-col justify-between overflow-y-auto border-2 rounded-lg  space-y-2 scrollbar">
+
+                        <button v-for="(unfin, key) in unfinish" @click="openForm(unfin.equipment, unfin.id)"
+                            class="flex justify-between  border-b  p-4 hover:bg-slate-200 border-grey-200 last:border-transparent">
+
+                            <div class="grid grid-cols-1 text-start">
+                                <span class="font-bold text-base text-gray-700 uppercase">{{ unfin.municipality
+                                }}</span>
+                                <span class="text-xs text-gray-700 uppercase">{{ unfin.equipment }}</span>
+                            </div>
+
+
+
+                        </button>
+
+
+                    </div>
+                </div>
+            </div>
+            <div class=" flex flex-col z-0  justify-between col-span-3">
+                <div class="flex flex-col  overflow-hidden h-full ">
+
+                    <form v-if="toggleForm" @submit.prevent="handleSubmit"
+                        class=" flex flex-col justify-between overflow-y-auto border-2 rounded-lg  space-y-2 scrollbar p-5 " 
+                        :class="toggleForm ? 'animate-fade-in-down' : 'animate-fade-out-up'">
+                        <div class="flex flex-row justify-between"> 
+                            <h1 class="font-bold text-2xl font-sans antialiased capitalize">
+                                {{ equipmentAttr.equipment_name }}</h1>
+
+                        </div>
+
+                        <div class="grid grid-cols-2 gap-10  h-full w-full">
+
+
+                            <div class="grid grid-rows-3 gap-10">
+                                <div class="flex flex-col">
+                                    <label class="text-sm font-bold">Serial Number</label>
+                                    <input type="number" v-model="equipmentAttr.serial_number"
+                                        class="border-b-2 border-0  px-2 py-1  focus:ring-0  focus:border-b-blue-600">
+                                </div>
+                                <div class="flex flex-col">
+                                    <label class="text-sm font-bold">Code</label>
+                                    <input type="text" v-model="equipmentAttr.code"
+                                        class="border-b-2 border-0  px-2 py-1  focus:ring-0  focus:border-b-blue-600">
+                                </div>
+                                <div class="flex flex-col">
+                                    <label class="text-sm font-bold">Asset ID</label>
+                                    <input type="number" v-model="equipmentAttr.asset_id"
+                                        class="border-b-2 border-0  px-2 py-1  focus:ring-0  focus:border-b-blue-600">
+                                </div>
+                            </div>
+
+                            <div class="grid grid-rows-3 gap-10">
+                                <div class="flex flex-col">
+                                    <label class="text-sm font-bold">Unit</label>
+                                    <input type="number" v-model="equipmentAttr.unit"
+                                        class="border-b-2 border-0  px-2 py-1  focus:ring-0  focus:border-b-blue-600">
+                                </div>
+                                <div class="flex flex-col">
+                                    <label class="text-sm font-bold">Model Number</label>
+                                    <input type="number" v-model="equipmentAttr.model_number"
+                                        class="border-b-2 border-0  px-2 py-1  focus:ring-0  focus:border-b-blue-600">
+                                </div>
+                                <div class="flex flex-col">
+                                    <label class="text-sm font-bold">Quantity</label>
+                                    <input type="number" v-model="equipmentAttr.quantity"
+                                        class="border-b-2 border-0  px-2 py-1  focus:ring-0  focus:border-b-blue-600">
+                                </div>
+                            </div>
+                            <div class="flex justify-end col-span-2 ">
+                                <button type="submit"
+                                    class="bg-orange-500 px-11 py-2 rounded-xl  hover:bg-orange-700 ">save</button>
+
+                            </div>
+
+
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </Content-box>
+
+</template>
+
+<script>
+import ContentBox from '@/Components/ContentBox.vue';
+import MunicipalityLayout from '@/Layouts/MunicipalityLayout.vue'
+import axios from 'axios';
+import { ref } from 'vue';
+import { Inertia } from '@inertiajs/inertia'
+import { Head, useForm } from '@inertiajs/inertia-vue3';
+
+
+export default {
+    layout: MunicipalityLayout,
+    props: {
+        notifications: Array,
+        unfinish: Array,
+    },
+    setup() {
+
+        const equipmentAttr = useForm({
+            id: null,
+            equipment_name: '',
+            serial_number: null,
+            unit: null,
+            code: '',
+            model_number: null,
+            asset_id: null,
+            quantity: null
+        })
+
+        const handleSubmit = async () => {
+            equipmentAttr.post(route('api.attrs'), {
+                onError: (e) => {
+                    alert(Object.values(e))
+                },
+                preserveScroll: true,
+                onFinish: ()=>{
+                    toggleForm.value = !toggleForm.value
+                }
+            })
+
+
+        }
+        const selectedEquipment = ref('')
+        const toggleForm = ref(false)
+
+        const openForm = (equipment, id) => {
+
+            equipmentAttr.equipment_name = equipment
+            equipmentAttr.id = id
+            selectedEquipment.value = equipment
+            toggleForm.value = !toggleForm.value
+        }
+
+        const accept = async (equip, id, notify_id, muni) => {
+            await axios.post(`/api/accepted`, {
+                equipment: equip,
+                borrower_id: id,
+                notif_id: notify_id,
+                municipality: muni
+            }).then((res) => {
+
+                Inertia.reload()
+            })
+        }
+
+        const deny = async (equip, id, notify_id, muni) => {
+            await axios.post(`/api/deny`, {
+
+                equipment_name: equip,
+                borrower_id: id,
+                notif_id: notify_id,
+                municipality: muni
+            }).then((res) => {
+
+                Inertia.reload({ only: ['notifications'] })
+            })
+        }
+
+        return {
+            accept,
+            deny,
+            equipmentAttr,
+            toggleForm,
+            openForm,
+            selectedEquipment,
+            handleSubmit,
+        };
+    },
+    components: { ContentBox, Head }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
